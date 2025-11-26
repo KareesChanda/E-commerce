0a4cb2158ecb61ffe7be1e6cad7b4b7bfe761665
mwamba <mwambachanda51@gmail.com>
2025-11-26 10:23:19 -0500

    created the chekoutpage shell and styling

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,5 +1,7 @@
-import HomePage from './pages/HomePage'
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router';
+import HomePage from './pages/HomePage';
+import CheckoutPage from './pages/CheckoutPage';
+
 import './App.css'
 
 function App() {
@@ -7,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<div>The check out Test Page</div>} />
+      <Route path="/checkout" element={<CheckoutPage />} />
 
     </Routes>
     
