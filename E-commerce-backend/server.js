7ee40e6a9d6bfb2b974f510146d8743bedd62fe8
mwamba <mwambachanda51@gmail.com>
2025-12-03 09:41:42 -0500

    Creating and expanding the backend structure. creating objects and mapping objects to respective entities.

--- E-commerce-backend/server.js
+++ E-commerce-backend/server.js
@@ -1,15 +1,32 @@
-import express from 'express';
+import express from "express";
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 //const express = reqiure('express');
 const app = express();
 const PORT = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //now put the above variables together to run the server
-app.listen(PORT, (error) =>{
-    if(!error){
-        console.log("Server is up and running successfully on Port "+PORT);
-        
-    }else{
-        console.log("Error occured, server can't start "+error);
-    }
+app.listen(PORT, (error) => {
+  if (!error) {
+    console.log("Server is up and running successfully on Port " + PORT);
+  } else {
+    console.log("Error occured, server can't start " + error);
+  }
 });
+
+//midddleware
+app.use(cors());
+app.use(express.json());
+
+// Serve images from the images folder
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+//Use routes
+app.use('/api/products', productRoutes);
+
+// Serve static files from the dist folder
+app.use(express.static(path.join(__dirname, 'dist')));
